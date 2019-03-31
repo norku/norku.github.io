5c6b561e0684b1ed62a4005708aa159a96110de3
Noora Kurko <noora.kurko@gmail.com>
2019-03-31 18:42:05 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,14 @@
 //   $(".main-nav").css("opacity", 0.9 -
 //   $(window).scrollTop() / 650);
 // });
+const $readMore = $(".readMore").append('<button class="btn">Read more</button>');
+const $btn = $(".btn");
+
+function testAlert() {
+  alert("Hi!");
+}
+
+// document.getElementByClass("readMore").addEventListener("click", testAlert());
+
+
+$btn.css("color", "blue");
